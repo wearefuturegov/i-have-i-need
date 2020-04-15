d13201ab75015aba83cc98088b1fe2f621b6218e
Tim Wisniewski <tim@timwis.com>
2020-04-15 13:42:10 +0100

    Use explicit config in passwordless and leave comment

--- config/initializers/passwordless.rb
+++ config/initializers/passwordless.rb
@@ -1,5 +1,5 @@
 config = Rails.configuration.councils[ENV['COUNCIL'] || :demo]
 Passwordless.default_from_address =  config[:default_from_address]
-# Passwordless.restrict_token_reuse = true
+Passwordless.restrict_token_reuse = false # Tokens can be reused until the magic link times out (timeout_at)
 Passwordless.expires_at = lambda { 1.day.from_now } # How long until a passwordless session expires.
 Passwordless.timeout_at = lambda { 1.hour.from_now } # How long until a magic link expires.
